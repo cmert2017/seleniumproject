fa631179842a23fda41f551f21de894db4468eee
cmert2017 <cihanmert2075@gmail.com>
2020-11-15 11:00:58 -0500

    day14_excel_io

--- src/test/java/com/cybertek/tests/day14_excel_io/ReadTextFile.java
+++ src/test/java/com/cybertek/tests/day14_excel_io/ReadTextFile.java
@@ -20,6 +20,25 @@ public class ReadTextFile {
             System.out.println(scanner.nextLine());
         }
 
+        System.out.println("########################################");
+
+        String path2 = "grade.csv";
+        File file2 = new File(path2);
+
+        System.out.println(file2.exists());
+        System.out.println(file2.isFile());
+
+
+        Scanner scanner2 = new Scanner(file2);
+
+        while(scanner2.hasNext()){
+            System.out.println(scanner2.nextLine());
+        }
+
+
+
+
+
     }
 
 
